db9523ab1253194f44f084f046d4fb8cd6413f5b
jrconlin <jconlin+git@mozilla.com>
2012-06-28 16:48:41 -0700

    Nightly checkin
    Added debugging
    fixed initial message injection
    fixed site name error
    Adding site controls

--- data/panel.js
+++ data/panel.js
@@ -1,11 +1,47 @@
 var notifications = [];
+var sites = [];
+var DB = {};
 
-self.port.on('message:init', function(messages) {
-  notifications = messages;
+var dumpObj = function(obj, name){
+  if (name) {
+    console.info("=== Dumping", name, typeof(obj));
+  }
+  switch (typeof(obj)) {
+    case 'object':
+      keys = Object.keys(obj);
+      for(var i=0;i<keys.length;i++){
+        console.info(' ',keys[i],
+          typeof(obj[keys[i]])=='function' ? '()' : JSON.stringify(obj[keys[i]]))
+      }
+      console.info('===');
+      break;
+    case 'string':
+    case 'number':
+      console.info(obj);
+      break;
+    default:
+      console.info(typeof(obj))
+  }
+}
+
+self.port.on('setDB', function(data) {
+  console.debug('setting DB');
+  DB = data;
+});
+
+self.port.on('message:init', function(data) {
+  dumpObj(data, 'panel->message:init.messages');
+  if (data.notifications){
+    console.debug('initializing notifications', data.notifications.length);
+    notifications = data.notifications;
+  }
   render();
 });
 
 self.port.on('message', function(message) {
+  console.debug('panel: recvd message', notifications.length);
+  dumpObj(message, 'panel:message');
+  dumpObj(notifications, 'panel:notifications');
   notifications.push(message);
   render();
 });
@@ -16,7 +52,9 @@ self.port.on('delete', function(index) {
 });
 
 document.addEventListener('click', function(e) {
+  var depth=0;
   for (var el = e.target; el.parentNode; el = el.parentNode) {
+    console.debug('click', depth++, JSON.stringify(e), JSON.stringify(el.classList));
     if (el.tagName.toLowerCase() == 'a') {
       e.preventDefault();
       self.port.emit('click', el.getAttribute('href'),
@@ -26,16 +64,34 @@ document.addEventListener('click', function(e) {
         self.port.emit('delete', el.getAttribute('data-index'));
       }
       return;
-    } else if (el.classList.contains('del')) {
+    }
+    if (el.classList.contains('del')) {
       e.preventDefault();
       self.port.emit('delete', el.parentNode.parentNode.getAttribute('data-index'));
       return;
-    } else if (el.classList.contains('header-link')) {
+    }
+    if (el.classList.contains('header-link')) {
       document.body.classList.toggle('flipped');
-    } else if (el.parentNode.id == 'tabs') {
+      return;
+    }
+    if (el.parentNode.id == 'tabs') {
       selectTab(el);
-    } else if (el.id == 'trash') {
+      return
+    }
+    if (el.id == 'trash') {
       clearAll();
+      return
+    }
+    if (el.classList.contains('pause-site')) {
+      site = el.getAttribute('data-id');
+      self.port.emit('pause', site);
+      return;
+    }
+    if (el.classList.contains('kill-site')) {
+      site = el.getAttribute('data-id');
+      console.debug('killing ', site);
+      self.port.emit('kill', site);
+      return;
     }
   }
 });
@@ -65,18 +121,21 @@ function selectTab(el) {
 function render() {
   var list = $('notifications'),
       template = $('notifications-template').textContent,
-      view = {sites: []};
+      view = {sites:[]},
+      sites=[];
 
+  dumpObj(notifications, 'rendering...');
   notifications.sort(function(a, b) {
     if (a.time < b.time) return 1;
     if (a.time > b.time) return -1;
     return 0;
   });
 
-  // Clean up the time element.
+  // Clean up the notifications.
   notifications.forEach(function(e, index) {
     e.prettyTime = prettyDate(e.time);
     e.index = index;
+    e.name = DB['domain:' + e.token] || 'Unknown';
   });
 
   // notifications.groupBy('site')
@@ -86,36 +145,47 @@ function render() {
     (groups[key] || (groups[key] = [])).push(e);
   });
 
-
   for (var domain in groups) {
-    var site = {domain: domain == 'Welcome' ? 'mozilla.org' : domain,
-                name: domain,
+    var site = {domain: domain == 'welcome' ? 'mozilla.org' : domain,
+                name: DB['domain:' + domain],
                 icon: icons[domain] || icons["default"]};
     site.notifications = groups[domain];
     view.sites.push(site);
   }
+  view['settings']['sites'] == DB.sites;
 
+  console.debug('render view', JSON.stringify(view),"\n DB", JSON.stringify(DB));
   list.innerHTML = Mustache.render(template, view);
-  renderSettings();
+  renderSettings(view);
 }
 
-function renderSettings() {
+function renderSettings(view) {
+  console.debug('renderSettings');
+  if (!$('site-settings-template')) {
+    return;
+  }
   var list = $('site-settings'),
       template = $('site-settings-template').textContent,
-      view = {sites: []},
       sites = {};
 
   notifications.forEach(function(e) {
-    var domain = e.site;
-    if (!(domain in sites)) {
-      view.sites.push({domain: domain,
+    // Add the site if it's not there.
+    if (!(e.site in sites)) {
+      if (e.site == undefined) {
+        console.debug("Undefined domain")
+        name = 'Unknown';
+        dumpObj(e, 'Unknown site element');
+      }
+      console.debug("Settings: Adding site", JSON.stringify(e.site));
+      view.sites.push({name: e.site || 'Unknown',
+                       token: e.token,
                        icon: icons[domain] || icons["default"],
                        latest: e.prettyTime});
-      sites[domain] = true;
-
+      sites[e.site] = true;
     }
   });
-
+  console.debug('notifications length', notifications.length);
+  console.debug('Rendering settings', JSON.stringify(view));
   list.innerHTML = Mustache.render(template, view);
 }
 
